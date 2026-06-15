54611492ee5c9f6438b8bb2f12866c08a8c1cfa5
m-alladi2006 <alladimanjunadh07@gmail.com>
2026-06-15 16:20:46 +0530

    PRODUCT MODEL (models/Product.js)

---
+++ backend/models/Product.js
@@ -0,0 +1,44 @@
+// backend/models/Product.js
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    category: {
+      type: String,
+    },
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
